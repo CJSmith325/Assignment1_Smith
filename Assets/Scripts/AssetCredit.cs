71308a9c6d442be6fa111299f30668db0e8b9864
CJSmith325 <90848885+CJSmith325@users.noreply.github.com>
2022-01-29 14:36:23 -0500

    Sprites and Movement

---
+++ Assets/Scripts/AssetCredit.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetCredit : MonoBehaviour
+{
+    // astronaut sprite https://assetstore.unity.com/packages/2d/characters/2d-character-astronaut-182650?aid=1011l7N9K&utm_campaign=unity_affiliate&utm_medium=affiliate&utm_source=partnerize-linkmaker#content 
+    
+    // enemy alien sprite 
+}
--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -1,17 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime;
+using System.Globalization;
+using System;
 
 public class Jump : MonoBehaviour
 {
+    [SerializeField] float movementSpeed = 6f;
+    
+
     public Rigidbody2D reggiebody;
     public float jumpForce;
     bool isGrounded = false;
     public float groundRayLength;
     public LayerMask layers;
+
+   
     
+
     void Update()
     {
+        
+
+        GameObject arm = GameObject.Find("arm");
+        GameObject gun = GameObject.Find("gun");
+        GameObject player = GameObject.Find("Player");
+
+        
+
         isGrounded = Physics2D.Raycast(transform.position, -transform.up, groundRayLength, layers);
         Debug.DrawRay(transform.position, -transform.up * groundRayLength, Color.red);
        
@@ -20,5 +37,25 @@ public class Jump : MonoBehaviour
             Debug.Log("Jump is pressed");
             reggiebody.AddForce(Vector2.up * jumpForce);
         }
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        reggiebody.velocity = new Vector2((horizontalInput * movementSpeed) + (verticalInput * movementSpeed), reggiebody.velocity.y);
+        
+        if (reggiebody.velocity.x > 0)
+        {
+            arm.transform.rotation = Quaternion.Euler(0, 180, 0);
+            gun.transform.rotation = Quaternion.Euler(0, 180, 0);
+            player.transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        if (reggiebody.velocity.x < 0)
+        {
+            arm.transform.rotation = Quaternion.Euler(0, 0, 0);
+            gun.transform.rotation = Quaternion.Euler(0, 0, 0);
+           //  player.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+       
     }
 }
